a9ce31cf2752fa3fec1c011b6e800f1c62e02f12
Didac Campanals Subirats <didac.cs@gmail.com>
2022-07-20 19:45:23 +0200

    feat: add practices for event hub and event grid

---
+++ AZ204/Practices/EventGrid/Program.cs
@@ -0,0 +1,27 @@
+﻿using Azure;
+using Azure.Messaging.EventGrid;
+
+const string topicEndpoint = "https://hrtopicdcs.northeurope-1.eventgrid.azure.net/api/events";
+const string subscricptionName = "iGZpFqLKIACPNqgLUxB4SxARm4gSVRYi6mlcEwn8pdw=";
+
+EventGridPublisherClient client = new EventGridPublisherClient(new Uri(topicEndpoint), new AzureKeyCredential(subscricptionName));
+
+
+async Task SendMessages(string fullName, string address)
+{
+    await client.SendEventAsync(new EventGridEvent(
+         subject: $"New Employee: {fullName}",
+         eventType: "Employees.Registration.New",
+         dataVersion: "1.0",
+         data: new
+         {
+             FullName = fullName,
+             Address = address
+         }
+     ));
+}
+
+await SendMessages("Mario Fernandez Ruiz", "Castelao 100, Hospitalet");
+await SendMessages("John Doe", "Massachussets");
+await SendMessages("Elenaita", "En su puñetera casa");
+await SendMessages("Darth Vader", "Estrella de la muerte");
---
+++ AZ204/Practices/EventHub/Program.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Azure.Messaging.EventHubs.Consumer;
+using System.Text;
+
+const string connectionString = "Endpoint=sb://eventhubdcs.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=lfZDrfUqVP5KjeciY6WZ9lztXXpRBxWw0CBf7Lqx3RU=";
+const string hubName = "event";
+
+async Task SendMessages(int numMessages)
+{
+    var producerClient = new EventHubProducerClient(connectionString, hubName);
+    var eventBatch = await producerClient.CreateBatchAsync();
+
+    for (int i = 0; i < numMessages; i++)
+    {
+        eventBatch.TryAdd(new EventData($"Event number {i}"));
+        Console.WriteLine($"Published Message {i}");
+        await Task.Delay(100);
+    }
+
+    await producerClient.SendAsync(eventBatch);
+
+    eventBatch.Dispose();
+}
+
+
+async Task ReceiveMessages()
+{
+    string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+    var producerClient = new EventHubConsumerClient(consumerGroup, connectionString, hubName);
+    var eventBatch = producerClient.ReadEventsAsync();
+
+    await foreach (var item in eventBatch)
+    {
+        var message = Encoding.UTF8.GetString(item.Data.EventBody);
+        Console.WriteLine($"Received message {message}");
+    }
+
+    await producerClient.DisposeAsync();
+}
+
+await SendMessages(3);
+await ReceiveMessages();
